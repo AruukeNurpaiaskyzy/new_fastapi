66c8b814aa6679f5f390d2591661bf87a7ed0eec
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2026-05-10 20:13:23 +0300

    starting from main

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from fastapi import FastAPI
-app = FastAPI()
-@app.get('/')
-def root():
-    return {'message': 'Hello FastAPI'}
+# from fastapi import FastAPI
+# app = FastAPI()
+# @app.get('/')
+# def root():
+#     return {'message': 'Hello FastAPI'}
+
+from pydantic import BaseModel
+from typing import List
+
+class Task(BaseModel):
+    id: int
+    title: str
+    completed: bool=False
+
+task_db = []
+@app.post("tasks/", response_model = Task)
+def create_task(task: Task):
+    tasks_db.append(task)
+    return task
+@app.get("tasks/", response_model=List(Task))
+def get_task():
+    return tasks_db
