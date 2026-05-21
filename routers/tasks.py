e72603690917cc64a897b6d521538be8d0e66c7c
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2026-05-21 20:45:03 +0300

    modifying in tasks2

--- routers/tasks.py
+++ routers/tasks.py
@@ -20,5 +20,23 @@ def get_tasks():
     return task_db
 
 @router.post("/", response_model=dict)
-
-
+def create_task(task:TaskCreate):
+    global current_id
+    new_task = TaskResponse(
+        id=current_id,
+        title=task.title,
+        description=task.description or "",
+        completed=False
+    )
+    task_db.append(new_task)
+    current_id +=1
+    return {"message": "здача создана", "task": new_task.dic()}
+@router.get("/{task_id}", response_model=TaskResponse)
+def get_task(task_id:int):
+    for task in task_id:
+        if task.id == task_id:
+            return task
+    raise HTTPException(status_code = 404, detail = "the task is not found " )
+    
+    
+    
