384b58039bb3a2b6f24b876103e18cb089e10231
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2026-05-23 14:01:25 +0300

    modifying auth.py3

--- routers/auth.py
+++ routers/auth.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from datetime import datetime, timedelta
 
-router = APIRouter (prefix="/auth", tags = ['auth'])
+router = APIRouter(prefix="/auth", tags=['auth'])
 
 class LoginRequest(BaseModel):
     email: str
@@ -21,32 +21,34 @@ fake_users = {
     }
 }
 
-@router.post("/login",  response_model=LoginResponse)
-def login (credentials: LoginRequest):
-    user = fake_users.get(credentials.email)
-    if not user or user ["password"] != credentials.password:
-        raise HTTPException(status_code=401, detail= "wrong password")
-    
-    fake_token = f"fake_token_{credentials.email}_{datetime.now()}"
 
+@router.post("/login", response_model=LoginResponse)
+def login(credentials: LoginRequest):
+    user = fake_users.get(credentials.email)
+    if not user or user["password"] != credentials.password:
+        raise HTTPException(status_code=401, detail="Wrong email and password")
+    
+
+    fake_token = f"fake_token_{credentials.email}_{datetime.now()}"
+    
     return {
         "access_token": fake_token,
         "token_type": "bearer",
-        "message": f"Welcome, {user['username']}!"
+        "message": f"Добро пожаловать, {user['username']}!"
     }
 
 class RegisterRequest(BaseModel):
-    username:str
+    username: str
     email: str
-    password:str
+    password: str
 
 @router.post("/register")
-def register(user_date: RegisterRequest):
+def register(user_data: RegisterRequest):
     if user_data.email in fake_users:
         raise HTTPException(status_code=400, detail="The user is already exists")
+    
     fake_users[user_data.email] = {
         "password": user_data.password,
         "username": user_data.username
     }
-
-    return {"message": " the registration has done successfully! you can login"}
+    return {"message": "The registration has successfully done! You can login"}
